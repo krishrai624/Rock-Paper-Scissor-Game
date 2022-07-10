b563b5305e07bf1a4ac212a3f343cc2cd78a2bff
Krish Rai <46421812+krishrai624@users.noreply.github.com>
2022-07-10 08:41:46 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 
 import random as rd
 
-print("Please enter the words given Rock as r, p as p and s as s\n")
+print("Please enter the words given Rock as r, Paper as p and Scissor as s\n")
 
 given = ["r", "p", "s"]
 play = 0
 player_point = 0
 com_point = 0
 game_over = 10
+draw_point = 0
 
 while play < 9:
     player = input("Player 1 choose between r, p and s to win: ", )
@@ -20,6 +21,8 @@ while play < 9:
 
     if player == com:
         print("Draw, No one receive point.\n")
+        draw_point = draw_point + 1
+        print("Draw point is ",draw_point)
     elif player == "r" and com == "p":
         com_point = com_point +1
         print(f"Player choose is {player} and computer choose {com}")
@@ -35,22 +38,22 @@ while play < 9:
     elif player == "p" and com == "r":
         player_point = player_point +1
         print(f"Player choose is {player} and computer choose {com}")
-        print("Computer wins\n")
+        print("Player wins\n")
     elif player == "s" and com == "r":
         com_point = com_point +1
         print(f"Player choose is {player} and computer choose {com}")
-        print("Player wins\n")
+        print("Computer wins\n")
     elif player == "s" and com == "p":
         player_point = player_point +1
         print(f"Player choose is {player} and computer choose {com}")
-        print("Computer wins\n")
+        print("Player wins\n")
     else:
         print("You enter invalid value.\n")
     print(f"Player point is {player_point} and Computer point is {com_point}\n")
 
     game_over = game_over -1
     print("Play left", game_over)
-print(f"Player point is {player_point} and Computer point is {com_point}\n")
+print(f"Player point is {player_point}\nComputer point is {com_point}\nDraw point is {draw_point}\n")
 if player_point == com_point:
     print("Draw, both gain equal point")
 elif player_point > com_point:
